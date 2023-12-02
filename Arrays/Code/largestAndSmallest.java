f04c7f710303be0a98b5b9500cdf98f69fb5ea68
Varun Yadav <yadav.varun056038@gmail.com>
2023-12-02 16:54:46 +0530

    Delete Arrays/Code directory

--- Arrays/Code/largestAndSmallest.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class largestAndSmallest {
-    static int[] kthSmallestAndLargest(int[] arr){
-        Arrays.sort(arr);
-        int[] ans={arr[k],arr[k]}
-        return ans;
-    }
-}
