e7eda2628468c0e2c9171467ebc586598f30ae7f
Varun Yadav <vy056038@gmail.com>
2024-08-10 20:17:29 +0530

    added evaluation of prefix and postfix and some conversion

---
+++ Stack/Conversion/infixToPrefix.java
@@ -0,0 +1,69 @@
+package Stack.Conversion;
+// Evaluate the prefix expression (Infix to Prefix)
+
+import java.util.*;
+
+public class infixToPrefix {
+    public static void main(String[] args) {
+        String infix = "9-(5+3)*4/6";
+        Stack<String> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for (int i = 0; i < infix.length(); i++) {
+            char ch = infix.charAt(i);
+            int ascii = (int) ch;
+            // ASCII value of '0' => 48 and '9' => 57
+            if (ascii >= 48 && ascii <= 57) {
+                String s = " " + ch;
+                val.push(s);
+            } else if (op.size() == 0 || ch == '(' || op.peek() == '(') {
+                op.push(ch);
+            } else if (ch == ')') {
+                while (op.peek() != '(') {
+                    // work
+                    String v2 = val.pop();
+                    String v1 = val.pop();
+                    char o = op.pop();
+                    String t = o + v1 + v2;
+                    val.push(t);
+                }
+                op.pop(); // '(' es ko hata diya
+            } else {
+                if (ch == '+' || ch == '-') {
+                    // work
+                    String v2 = val.pop();
+                    String v1 = val.pop();
+                    char o = op.pop();
+                    String t = o + v1 + v2;
+                    val.push(t);
+                    // push
+                    op.push(ch);
+                }
+                if (ch == '*' || ch == '/') {
+                    if (op.peek() == '*' || op.peek() == '/') {
+                        // work
+                        String v2 = val.pop();
+                        String v1 = val.pop();
+                        char o = op.pop();
+                        String t = o + v1 + v2;
+                        val.push(t);
+                        // push
+                        op.push(ch);
+                    } else {
+                        op.push(ch);
+                    }
+                }
+            }
+        }
+        // Now make the val stack's size = 1 and op stack's size = 0
+        while (val.size() > 1) {
+            String v2 = val.pop();
+            String v1 = val.pop();
+            char o = op.pop();
+            String t = o + v1 + v2;
+            val.push(t);
+        }
+        String prefix = val.pop();
+        System.out.println("The Prefix value is : " + prefix);
+    }
+}
