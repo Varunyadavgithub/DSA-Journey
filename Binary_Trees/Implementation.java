0b89aeb815fe4e505b3045b11a469e0e083071bf
Varun Yadav <yadav.varun056038@gmail.com>
2025-02-02 12:05:02 +0530

    added Implementation of Binary Trees

---
+++ Binary_Trees/Implementation.java
@@ -0,0 +1,20 @@
+package Binary_Trees;
+
+/* Implementation of Binary Trees */
+
+public class Implementation {
+    public static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        System.out.println(root.val);
+    }
+}
