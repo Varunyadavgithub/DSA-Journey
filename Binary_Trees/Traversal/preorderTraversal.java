4bccff274ff4bd7f536ddb93275ce7b76a878cd7
Varun Yadav <yadav.varun056038@gmail.com>
2025-02-02 15:02:27 +0530

    added preorderTraversal

---
+++ Binary_Trees/Traversal/preorderTraversal.java
@@ -0,0 +1,56 @@
+package Binary_Trees.Traversal;
+
+/* Preorder Traversal. */
+
+public class preorderTraversal {
+    public static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void preorder(Node root) {
+        // Preorder => root -> left -> right
+        if (root == null)
+            return;
+        System.out.print(root.val + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(7);
+        Node b = new Node(9);
+
+        root.left = a;
+        root.right = b;
+
+        Node c = new Node(2);
+        Node d = new Node(6);
+
+        a.left = c;
+        a.right = d;
+
+        Node e = new Node(8);
+        Node f = new Node(7);
+
+        d.left = e;
+        d.right = f;
+
+        Node g = new Node(0);
+
+        b.left = null;
+        b.right = g;
+
+        Node h = new Node(13);
+
+        g.left = h;
+        g.right = null;
+        preorder(root);
+    }
+}
